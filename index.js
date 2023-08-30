16f973762ad46b07e1f956f760e6aa9d10ba4c53
Alf Nielsen <alfnielsen@gmail.com>
2023-08-30 12:29:48 +0200

    Fix hash check, when other extensions are involved

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ class AddSimpleSingleSpaBuildInfo {
       let packageJson = readFileSync(packageJsonPath, "utf8")
       return JSON.parse(packageJson)
     },
-    getName: (packageJson) => packageJson.name,
-    getVersion: (packageJson) => packageJson.version,
-    getTime: (packageJson) => new Date().toISOString(),
-    getFileName: (packageName) => packageName.replace(/@/, "").replace(/[\\\/]/g, "-"),
-    getFilePath: (packageFileName) => {
+    getName: packageJson => packageJson.name,
+    getVersion: packageJson => packageJson.version,
+    getTime: packageJson => new Date().toISOString(),
+    getFileName: packageName => packageName.replace(/@/, "").replace(/[\\\/]/g, "-"),
+    getFilePath: packageFileName => {
       let dir = this.options.getPath()
       return path.join(dir, "dist", `${packageFileName}.js`)
     },
     generateBuildInfo: ({ name, version, time, packageJson, base64Hash, content }) =>
       `//@build-info|${name}|${version}|${time}|${base64Hash}\n`,
-    postBuild: (compilation) => {
+    postBuild: (compilation, currectBuildInfo) => {
       // Get the package.json file
       const packageJson = this.options.getPackageJson()
       // Get the name of the package
@@ -42,11 +42,8 @@ class AddSimpleSingleSpaBuildInfo {
       const filePath = this.options.getFilePath(fileName)
       // load the file
       let content = readFileSync(filePath, "utf8")
-      let currectHash = ""
-      // Check if build info exists
+      // Remove current build info if exists
       if (content.indexOf("//@build-info|") === 0) {
-        // get currect hash
-        currectHash = content.split("|").reverse()[0]
         // remove the old build info
         content = content.replace(/\/\/@build-info\|.*\n/, "")
       }
@@ -55,8 +52,10 @@ class AddSimpleSingleSpaBuildInfo {
       hashSum.update(content)
       const base64Hash = hashSum.digest("base64")
       // test if first line is build info and it it's hash is the same as the current hash
-      if (currectHash === base64Hash) {
-        // No update!
+      if (currectBuildInfo && currectBuildInfo.indexOf(base64Hash) > 0) {
+        // Remove the build info (original)
+        content = currectBuildInfo + "\n" + content
+        writeFileSync(filePath, content)
       } else {
         // Generate the build info and add to top of file
         content = this.options.generateBuildInfo({ name, version, time, packageJson, base64Hash, content }) + content
@@ -71,8 +70,22 @@ class AddSimpleSingleSpaBuildInfo {
     }
   }
   apply(compiler) {
-    compiler.hooks.afterEmit.tap(pluginName, (compilation) => {
-      this.options.postBuild(compilation)
+    let currectBuildInfo = ""
+    compiler.hooks.beforeRun.tap(pluginName, compilation => {
+      const packageJson = this.options.getPackageJson()
+      const name = this.options.getName(packageJson)
+      const fileName = this.options.getFileName(name)
+      const filePath = this.options.getFilePath(fileName)
+      if (existsSync(filePath)) {
+        let content = readFileSync(filePath, "utf8")
+        // Check if build info exists
+        if (content.indexOf("//@build-info|") === 0) {
+          currectBuildInfo = content.split("\n")[0]
+        }
+      }
+    })
+    compiler.hooks.afterEmit.tap(pluginName, compilation => {
+      this.options.postBuild(compilation, currectBuildInfo)
     })
   }
 }
